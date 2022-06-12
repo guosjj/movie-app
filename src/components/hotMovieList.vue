44ac9d31492250e71a3ee77e933fe1640077dfd5
“guosj” <2547361543@qq.com>
2022-06-12 09:24:40 +0800

    添加下拉列表加载

--- src/components/hotMovieList.vue
+++ src/components/hotMovieList.vue
@@ -1,11 +1,16 @@
 <template>
-            <div class="bg-white mt-[5px]">
+            <van-list class="bg-white mt-[5px]"
+            v-model:loading="isLoading"
+            loading-text="正在加载电影数据"
+            finished-text="我是有底线的"
+            @load="loadNextPage"
+            :finished="queryParams.pageIndex>=resultData.pageCount">
                 <div class="flex h-[120px] p-[5px]" v-for="item in resultData.listData" :key="item.id">
                     <div class="w-[95px]">
-                        <img class="w-full h-full" :src="baseURL+item.movie_pc" alt="">
+                        <img @click="ImagePreview([baseURL+item.movie_pc])" class="w-full h-full" :src="baseURL+item.movie_pc" alt="">
                     </div>
                     <ul class="flex flex-col flex-1 justify-between text-[12px] list-none px-[10px]">
-                        <li class="text-[18px] font-bold">{{item.movie_name}}</li>
+                        <li class="text-[18pxs] font-bold">{{item.movie_name}}</li>
                         <li >众人评分: <span class="text-[14px] font-bold text-warningColor">{{item.movie_score}}</span></li>
                         <li >主演: {{item.role}}</li>
                         <li >上映时间:{{item.publicTime}}</li>
@@ -15,12 +20,12 @@
                         <button type="button" class="btn-presale" v-else>预告</button>
                     </div>
                 </div>
-            </div>
+            </van-list>
 </template>
 <script setup>
 import API from "../utils/API/index.js";
 import { ref,reactive,inject } from "vue";
-
+import { ImagePreview } from "vant";
 const baseURL = inject("baseURL");
 //定义请求参数
 const queryParams = reactive({
@@ -33,15 +38,31 @@ const resultData = reactive({
     listData: [],
     pageCount: 0
 });
+const isLoading = ref(false);
 const queryData = ()=>{
     API.movieInfo.getListByPage(queryParams).then((result)=>{
-        resultData.pageCount = result.data.pageCount;
-        resultData.listData = result.data.listData;
+        //原来的数据包含加载的数据
+        //resultData.listData = result.data.listData;
+        resultData.listData = resultData.listData.concat(result.data.listData)
+        resultData.pageCount = result.data.pageCount; 
         console.log(resultData);
+    })
+    .finally(()=>{
+        //数据请求完了，就把加载状态取消
+        isLoading.value = false;
     });
 };
 
 queryData();
+
+const loadNextPage =()=>{
+    console.log(new Date(),"加载下一页");
+    //第一步把页码加1
+    queryParams.pageIndex++;
+    //第二步：再次调用方法，查询数据
+    queryData();
+}
+
 </script>
 <style scoped lang="scss">
 .btn-buy{
